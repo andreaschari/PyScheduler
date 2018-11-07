7d8e2e041ac3e2c3b1a48f0b6fa336852386d11a
Andreas Chari <andreas9810@outlook.com>
2018-11-07 09:35:45 +0000

    Added initial files
    Initial Python files added

---
+++ schedulers.py
@@ -0,0 +1,33 @@
+import math
+
+from des import SchedulerDES
+from event import Event, EventTypes
+from process import ProcessStates
+
+class FCFS(SchedulerDES):
+        def scheduler_func(self, cur_event):
+                pass
+
+        def dispatcher_func(self, cur_proc):
+                pass
+
+class SJF(SchedulerDES):
+        def scheduler_func(self, cur_event):
+                pass
+
+        def dispatcher_func(self, cur_proc):
+                pass
+
+class RR(SchedulerDES):
+        def scheduler_func(self, cur_event):
+                pass
+
+        def dispatcher_func(self, cur_proc):
+                pass
+
+class SRTF(SchedulerDES):
+        def scheduler_func(self, cur_event):
+                pass
+
+        def dispatcher_func(self, cur_proc):
+                pass
